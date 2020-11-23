f99c9b0cdfd7481c3dc8adf6c0f1f83c2217f3ee
Dhrumil <vdhrumil5424@gmail.com>
2020-11-23 15:27:01 +1100

    Secret number is now random between 0 - 10

--- guess_game.py
+++ guess_game.py
@@ -1,11 +1,16 @@
-secret_number = 5
+from random import randint
+
+value = randint(0,10)
 guess_count = 0
 guess_limit = 3
+
 while guess_count < guess_limit:
-    guess = int(input("Guess: "))
+    guess = int(input("Hint: 0 - 10 | Guess: "))
     guess_count += 1
-    if guess == secret_number:
+    if guess == value:
         print("You won!")
+        print(f"Secret number was {value}!")
         break
 else:
     print("Out of guesses :(")
+    print(f"Secret number was {value}!")
